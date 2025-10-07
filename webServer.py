43e08aaa5802ee6725fc85a18c89fe690a745090
cherishnyu <cga8081@nyu.edu>
2025-10-07 16:36:10 -0400

    webServer.py

--- webServer.py
+++ webServer.py
@@ -3,68 +3,77 @@ from socket import *
 # In order to terminate the program
 import sys
 
-
 def webServer(port=13331):
-    serverSocket = socket(AF_INET, SOCK_STREAM)
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  
+  #Prepare a server socket
+  serverSocket.bind(("", port))
+  
+  #Fill in start
+  serverSocket.listen(1)
+  #Fill in end
 
-    # Prepare a server socket
-    serverSocket.bind(("", port))
+  while True:
+    #Establish the connection
+    
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    
+    try:
+      message = connectionSocket.recv(1024).decode()
+      filename = message.split()[1]
+      
+      #opens the client requested file.
+      f = open(filename[1:], 'rb')
+      
+      file_content = f.read()
+      content_length = len(file_content)
 
-    # Fill in start
-    serverSocket.listen(1)
-    # Fill in end
+      # 200 OK Response Headers
+      outputdata = b"HTTP/1.1 200 OK\r\n"
+      outputdata += b"Server: SimplePythonServer\r\n" # Added Server
+      outputdata += b"Connection: close\r\n"          # Added Connection
+      outputdata += b"Content-Length: " + str(content_length).encode() + b"\r\n" # Added Content-Length
+      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
 
-    while True:
-        # Establish the connection
+      #Fill in start - append your html file contents
+      outputdata += file_content
+      #Fill in end
+      
+      #Send the content of the requested file to the client
+      # Fill in start
+      connectionSocket.sendall(outputdata)
+      # Fill in end
+      
+      connectionSocket.close() #closing the connection socket
+      
+    except IOError:
+      # Send response message for file not found (404)
+      #Fill in start
+      not_found_body = b"<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1><p>The requested file was not found on this server.</p></body></html>"
+      not_found_length = len(not_found_body)
 
-        print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept()
+      not_found_header = b"HTTP/1.1 404 Not Found\r\n"
+      not_found_header += b"Server: SimplePythonServer\r\n" # Added Server
+      not_found_header += b"Connection: close\r\n"          # Added Connection
+      not_found_header += b"Content-Length: " + str(not_found_length).encode() + b"\r\n" # Added Content-Length
+      not_found_header += b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+      
+      connectionSocket.sendall(not_found_header + not_found_body)
+      #Fill in end
 
-        try:
-            message = connectionSocket.recv(1024).decode()
-            filename = message.split()[1]
 
-            # opens the client requested file.
-            f = open(filename[1:], 'rb')
+      #Close client socket
+      #Fill in start
+      connectionSocket.close()
+      #Fill in end
 
-            outputdata = b"HTTP/1.1 200 OK\r\n"
-            outputdata += b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+    except Exception as e:
+      print(f"An unexpected error occurred: {e}")
+      connectionSocket.close()
 
-            # Fill in start - append your html file contents
-            outputdata += f.read()
-            # Fill in end
-
-            # Send the content of the requested file to the client
-            # Fill in start
-            connectionSocket.sendall(outputdata)
-            # Fill in end
-
-            connectionSocket.close()  # closing the connection socket
-
-        except IOError:
-            # Send response message for file not found (404)
-            # Fill in start
-            not_found_header = b"HTTP/1.1 404 Not Found\r\n"
-            not_found_header += b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
-            not_found_body = b"<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1><p>The requested file was not found on this server.</p></body></html>"
-
-            connectionSocket.sendall(not_found_header + not_found_body)
-            # Fill in end
-
-            # Close client socket
-            # Fill in start
-            connectionSocket.close()
-            # Fill in end
-
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-            connectionSocket.close()
-
-    # Commenting out the below (some use it for local testing).
-    # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING
-    # serverSocket.close()
-    # sys.exit() # Terminate the program after sending the corresponding data
-
+  #serverSocket.close()
+  #sys.exit() 
 
 if __name__ == "__main__":
-    webServer(13331)
+  webServer(13331)
